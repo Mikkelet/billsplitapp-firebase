0535315a0cb521a821be013e199d5656580913cc
MikkelThygesen <Mikkelet@gmail.com>
2023-11-10 18:27:56 +0700

    migrated to v7

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -76,7 +76,7 @@ app.all("*", functions.https.onRequest(async (_, res) => {
     res.status(404).send("Invalid request")
 }))
 
-export const v6 = functions.https.onRequest(app)
+export const v7 = functions.https.onRequest(app)
 
 export const scheduledServicesV6 = functions.pubsub
     .schedule("0 0 1 * *")
